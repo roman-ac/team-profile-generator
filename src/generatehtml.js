eb90566828605c5b783348384e1ba42f76783635
Roman Ac <roman25xx@gmail.com>
2022-02-13 20:16:18 +1100

    fix error generate html

--- src/generatehtml.js
+++ src/generatehtml.js
@@ -1,18 +1,8 @@
-// generateManagers, generateEngineers, and generateInterns
+// creating generateHTML function
 
-function generateManagers(manager){
-    return `<div class="card" style="width: 285px;"><div class="card-header">${manager.role}</div><ul class="list-group list-group-flush"><li class="list-group-item">Name: ${manager.name}</li><li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></ali><li class="list-group-item">Employee ID: ${manager.id}</li><li class="list-group-item">Office Number: ${manager.officeNum}</li></ul></div><br />`
-}
 
-function generateEngineers(engineer){
-    return `<div class="card" style="width: 285px;"><div class="card-header">${engineer.role}</div><ul class="list-group list-group-flush"><li class="list-group-item">Name: ${engineer.name}</li><li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></ali><li class="list-group-item">Employee ID: ${engineer.id}</li><li class="list-group-item">Github: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.name}'s GitHub Profile</a></li></ul></div><br />`
-}
 
-function generateInterns(intern){
-    return `<div class="card" style="width: 285px;"><div class="card-header">${intern.role}</div><ul class="list-group list-group-flush"><li class="list-group-item">Name: ${intern.name}</li><li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></ali><li class="list-group-item">Employee ID: ${intern.id}</li><li class="list-group-item">School: ${intern.school}</li></ul></div><br />`
-}
-
-function generateHTML(managers, engineers, interns) {
+function generateHTML(manager, engineer, intern) {
     return`<!DOCTYPE html>
         <html lang="en">
         <head>
@@ -30,20 +20,47 @@ function generateHTML(managers, engineers, interns) {
         <div class="container">
             <div class="row">
                 <div class="col">
-                <h2>Managers</h2>
-                <hr>
-                ${managers.map(generateManagers).join('\n')}
+                    <h2>Managers</h2>
+                    <hr>
+                    <div class="card" style="width: 285px;"><div class="card-header">${manager.role}</div>
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Name: ${manager.name}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></ali>
+                    <li class="list-group-item">Employee ID: ${manager.id}</li>
+                    <li class="list-group-item">Office Number: ${manager.officeNum}</li>
+                    </ul>
+                    </div>
+                    <br />
                 </div>
+
                 <div class="col">
-                <h2>Engineers</h2>
-                <hr>
-                ${engineers.map(generateEngineers).join('\n')}
+                    <h2>Engineers</h2>
+                    <hr>
+                    <div class="card" style="width: 285px;"><div class="card-header">${engineer.role}</div>
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Name: ${engineer.name}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></ali>
+                    <li class="list-group-item">Employee ID: ${engineer.id}</li>
+                    <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.name}'s GitHub Profile</a></li>
+                    </ul>
+                    </div>
+                    <br />
                 </div>
+
                 <div class="col">
-                <h2>Interns</h2>
-                <hr>
-                ${interns.map(generateInterns).join('\n')}
+                    <h2>Interns</h2>
+                    <hr>
+                    <div class="card" style="width: 285px;"><div class="card-header">${intern.role}</div>
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Name: ${intern.name}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></ali>
+                    <li class="list-group-item">Employee ID: ${intern.id}</li>
+                    <li class="list-group-item">School: ${intern.school}</li>
+                    </ul>
+                    </div>
+                    <br />
                 </div>
+
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
